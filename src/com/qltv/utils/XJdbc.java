01e66e9aa5bb8d5ec95b7b3772930deab9b64d1e
Admin <Admin@NGUYENPHUOCVINH>
2023-12-05 19:16:25 +0700

    Int First

--- src/com/qltv/utils/XJdbc.java
+++ src/com/qltv/utils/XJdbc.java
@@ -22,10 +22,10 @@ public class XJdbc {
 
     private static String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
     public static String connUrl 
-            = "jdbc:sqlserver://NGUYENPHUOCVINH:1433;"
+            = "jdbc:sqlserver://LAPTOP-SSMVGE81:1433;"
             + "database=QuanLyThuVien;"
             + "user=sa; "
-            + "password=123456; "
+            + "password=123456789; "
             + "encrypt=false;"
             + "trustServerCertificate=true;";;
 
